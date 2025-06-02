a6b726d912b9631067636b8878c3fc92d6e3b4f6
BilkisShakila <bilkisshakilaa@gmail.com>
2025-06-02 18:31:11 +0700

    membuat class lia

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -11,3 +11,21 @@ public:
     }
 };
 
+class joko : public seseorang
+{
+public: 
+    void pesan()
+    {
+        cout << "Pesan dari joko " << endl;
+    }
+};
+
+class Lia : public seseorang
+{
+public: 
+    void pesan()
+    {
+        cout << "Pesan dari Lia " << endl;
+    }
+};
+
